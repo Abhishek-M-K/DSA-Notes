aad79fcca0a7c5b74cbdaaba00a17089e7ed7d60
Abhishek-M-K <khandareabhishek98@gmail.com>
2023-12-26 13:38:19 +0530

    ongoing: bfs

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,33 @@
+/*
+                                                                      BFS TRAVERSAL OF A GRAPH
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        int vis[V]={0};
+        vis[0]=1;
+        queue<int> q;
+        q.push(0); //zero-based
+
+        vector<int> bfs;
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            bfs.push_back(node);
+
+            for(auto it:adj[node]){
+                if(vis[it]!=1){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
+
+
+*/
